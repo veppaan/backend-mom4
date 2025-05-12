80085074004782982f2ef4edef2923aeb19d7f2d
Vera Kippel <veki2400@student.miun.se>
2025-05-12 19:39:17 +0200

    funktion kollar token

--- server.js
+++ server.js
@@ -11,6 +11,25 @@ app.use(bodyParser.json());
 //Routes
 app.use("/api", authRoutes);
 
+//Protected routes
+app.get("/api/protected", authenticateToken, (req, res) => {
+    res.json({ message: "Skyddad route!"});
+});
+//Validate token
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; //Token
+
+    if(token == null) res.status(401).json({ message: "Not authorized for this route - token missing!"});
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, username) =>{
+        if(err) return res.status(403).json({ message: "Invalid JWT"});
+
+        req.username = username;
+        next();
+    })
+}
+
 //Start application
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
